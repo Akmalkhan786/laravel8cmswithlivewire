6af0939a15a449b6a3c6d3b40671c783f41ad67a
Akmal Khan <khan.akmal542@gmail.com>
2021-01-21 01:46:02 +0500

    Create the page Modal

--- app/Http/Livewire/Pages.php
+++ app/Http/Livewire/Pages.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Page;
+use Illuminate\Validation\Rule;
 use Livewire\Component;
 
 class Pages extends Component
@@ -11,12 +13,75 @@ class Pages extends Component
     public $title;
     public $content;
 
+    /*
+     * Validation rules
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required',
+            'slug' => ['required', Rule::unique('pages', 'slug')],
+            'content' => 'required',
+        ];
+    }
+
+    /*
+     * Create Page Model
+     */
     public function createShowModal()
     {
         $this->modalFormVisible = true;
     }
+
+    /*
+     * Create the page
+     */
+    public function create()
+    {
+        $this->validate();
+        Page::create($this->modelData());
+        $this->modalFormVisible = false;
+        $this->reset();
+    }
+
+    /*
+     * Read the data
+     */
+    public function read()
+    {
+        return Page::paginate(5);
+    }
+
+    /*
+     * Auto populate the slug field
+     */
+    public function updatedTitle($value)
+    {
+        $this->generateSlug($value);
+    }
+
+    private function generateSlug($value)
+    {
+        $process1 = str_replace(' ', '-', $value);
+        $process2 = strtolower($process1);
+        $this->slug = $process2;
+    }
+
+    /*
+     * Get form data
+     */
+    public function modelData()
+    {
+        return [
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'content' => $this->content,
+        ];
+    }
     public function render()
     {
-        return view('livewire.pages');
+        return view('livewire.pages', [
+            'data' => $this->read(),
+        ]);
     }
 }
